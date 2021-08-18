7643f392a702f965d7f23e1db5593c073e24d66b
urmil89 <urmil8989@gmail.com>
2021-08-18 19:32:50 +0530

    bug->main.py'

--- get_words.py
+++ get_words.py
@@ -1,29 +1,35 @@
-
 import requests
 import random
-global world
+import time
+global word
+global definition
+global audio
 
-url_word = "https://random-word-api.herokuapp.com/word?number=" + \
+def VocabReminder():
+    url_word = "https://random-word-api.herokuapp.com/word?number=" + \
     str(random.randint(1, 1))
+    response_word = requests.request("GET", url_word)
+    # print(response_word.status_code)
 
-response_word = requests.request("GET", url_word)
-# print(response_word.status_code)
+    for word in response_word.json():
+        print(f"word : {word}")
+    # find meaning of random word
 
-for word in response_word.json():
-    print(f"word : {word}")
+    url_meaning = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
+    response2_meaning = requests.request("GET", url_meaning)
+    data = response2_meaning.json()
 
-# find meaning of random word
+    try:
+        definition = data[0]['meanings'][0]['definitions'][0]['definition']
+        audio = data[0]['phonetics'][0]['audio']
+        print(f"definition : {definition}")
+        print(f"audio : {audio}")
+    except:
+        print("No definition found")
+        definition = "No definition found"
 
-url_meaning = "https://api.dictionaryapi.dev/api/v2/entries/en/" + word
-response2_meaning = requests.request("GET", url_meaning)
-data = response2_meaning.json()
 
-try:
-    definition = data[0]['meanings'][0]['definitions'][0]['definition']
-    print(f"definition : {definition}")
-
-    audio = data[0]['phonetics'][0]['audio']
-    print(f"audio : {audio}")
-
-except:
-    print("No definition found")
+if __name__ == '__main__':
+    while True:
+        VocabReminder()        
+        time.sleep(0.25*60)
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import time
 from plyer import notification
-import get_words as w
 from playsound import playsound
+# from get_words import VocabReminder
+import get_words
 
+# print(get_words.VocabReminder.word)
+# print(get_words.VocabReminder.__getattribute__('word'))
+exit()
 if __name__ == '__main__':
     while True:
-        playsound(w.audio)
         notification.notify(
-            title=w.word,
-            message=w.meaning,
+            title=get_words.VocabReminder.word,
+            message=get_words.VocabReminder.definition,
+            app_icon="E:\\CODES\\Vocab-Reminder\\dictionary.ico",
             timeout=12
         )
         #	time.sleep(6)
-        time.sleep(5*60)
+        time.sleep(0.5*60)
